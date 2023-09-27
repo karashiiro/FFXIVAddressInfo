fbe62d6e82b796a8b907cd5b47157559e1252468
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-09-27 07:24:11 -0700

    Create version bumper

---
+++ FFXIVAddressInfo.VersionBumper/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Construction;
+
+var inFile = args.Length == 0 ? "../../../../FFXIVAddressInfo/FFXIVAddressInfo.csproj" : args[0];
+
+var project = ProjectRootElement.Open(inFile);
+ArgumentNullException.ThrowIfNull(project);
+
+foreach (var property in project.Properties)
+{
+    if (property.Name != "Version")
+    {
+        continue;
+    }
+
+    var version = Version.Parse(property.Value);
+    Console.WriteLine($"Bumping version from {version} to {version.Major}.{version.Minor}.{version.Build + 1}");
+
+    property.Value = $"{version.Major}.{version.Minor}.{version.Build + 1}";
+}
+
+project.Save();
